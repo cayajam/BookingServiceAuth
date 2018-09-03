55e92044a3b8c40957f99543c6acfb74a9b8d899
User <janin.cayanan@pointwest.com.ph>
2018-09-03 17:27:21 +0800

    201809031727

--- src/main/java/pw/io/booker/aop/AuthenticationAspect.java
+++ src/main/java/pw/io/booker/aop/AuthenticationAspect.java
@@ -1,8 +1,12 @@
 package pw.io.booker.aop;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Service;
 
 import pw.io.booker.exception.CustomException;
@@ -12,6 +16,8 @@ import pw.io.booker.repo.AuthenticationRepository;
 @Service
 public class AuthenticationAspect {
 	
+	Logger logger = Logger.getLogger(AuthenticationAspect.class);
+	
 	private AuthenticationRepository authenticationRepository;
 
 	public AuthenticationAspect(AuthenticationRepository authenticationRepository) {
@@ -19,18 +25,32 @@ public class AuthenticationAspect {
 		this.authenticationRepository = authenticationRepository;
 	}
 	
+	@Before("execution(* pw.io.booker.controller..*(..))")
+	public void beforeMethod(JoinPoint joinPoint) {
+		
+		logger.info("Start of method" + joinPoint.getClass().getName());
+	}
+	
 	@Around("execution(* pw.io.booker.controller..*(..)) && args(token,..)")
 	public Object checkAccess(ProceedingJoinPoint jointPoint, String token) throws Throwable {
 		
 		if (token == null) {
+			logger.error("Invalid token.");
 			throw new CustomException("Invalid token.");
 		}
 		
 		if (authenticationRepository.findByToken(token) == null) {
+			logger.error("Token does not exist.");
 			throw new CustomException("Token does not exist.");
 		}
 		
 		return jointPoint.proceed();
+	}
+	
+	@After("execution(* pw.io.booker.controller..*(..))")
+	public void afterMethod(JoinPoint joinPoint) {
+		
+		logger.info("End of method" + joinPoint.getClass().getName());
 	}
 
 }
